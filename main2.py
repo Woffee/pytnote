85b281780ccb8eab6f23d5802e391792e4ea155e
woffee <iwoffee@gmail.com>
2019-09-05 21:23:37 -0400

    add log and mysql

---
+++ main2.py
@@ -0,0 +1,75 @@
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, send_from_directory, jsonify, request, redirect
+import time
+import logging
+import os
+import json
+import configparser
+from note import Note
+
+app = Flask(__name__)
+
+noteObj = None
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+config = configparser.ConfigParser()
+config.read(BASE_DIR + '/.ini')
+
+today = time.strftime("%Y-%m-%d", time.localtime())
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(filename)s line: %(lineno)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename=BASE_DIR + '/log/tnote-' + today + '.log')
+
+
+@app.route("/")
+def index():
+    global noteObj
+    notes = []
+
+    data = noteObj.getLast24hrsNotes()
+    for d in data:
+        item = {}
+        item['no'] = d[0]
+        item['note'] = d[1]
+        item['created'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(d[2]))
+        item['ishref'] = True if item['note'].find('http') > -1 else False
+        notes.append(item)
+
+    return render_template('index.html', notes=notes)
+
+
+@app.route("/add", methods=['POST'])
+def add():
+    nn = request.form['note']
+    if nn:
+        noteObj.addNote(nn)
+    return redirect('/')
+
+
+def checkMysql():
+    global noteObj,config
+    if noteObj:
+        logging.info("MySQL already connected")
+        return
+    try:
+        noteObj = Note(config['MYSQL']['HOST'],
+                config['MYSQL']['USER'],
+                config['MYSQL']['PASSWORD'],
+                config['MYSQL']['DATABASE'])
+    except Exception as e:
+        logging.error("Connect MySQL error: "+str(e))
+        exit(0)
+
+
+app.before_request(checkMysql)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=True, port=8070)
+
+
+
+
+
+
---
+++ note.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+import pymysql
+import time
+import configparser
+import os
+import logging
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+today = time.strftime("%Y-%m-%d", time.localtime())
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(filename)s line: %(lineno)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename=BASE_DIR + '/logs/tnote-' + today + '.log')
+
+
+class Note:
+    def __init__(self, host, user, pwd, database):
+        try:
+            self.db = pymysql.connect(host, user, pwd, database)
+        except Exception as e:
+            logging.error(str(e))
+
+    def getLast24hrsNotes(self):
+        last_time = int(time.time()) - 24*3600
+        sql = "select id,note,create_time from tnote where create_time >= %d" % (last_time)
+        # print(sql)
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(sql)
+            data = cursor.fetchall()
+            return data
+        except Exception as e:
+            print("get notes error:" + str(e))
+        return []
+
+    def addNote(self, note):
+        nowtime = int(time.time())
+        sql = "insert into tnote (note, create_time) values ('%s', %d)" % (note, nowtime)
+        # print(sql)
+
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(sql)
+            self.db.commit()
+        except Exception as e:
+            logging.error("insert note error:" + str(e))
+            self.db.rollback()
+
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read(BASE_DIR + '/.ini')
+
+    note = Note(config['MYSQL']['HOST'],
+            config['MYSQL']['USER'],
+            config['MYSQL']['PASSWORD'],
+            config['MYSQL']['DATABASE'])
+
